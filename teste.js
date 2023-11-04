40d267c43c30f7a7f9671ede3678661b3d547fc6
levoratolucas <lucaslevorato@gmail.com>
2023-11-04 03:27:52 -0300

    modified:   index.html modified:   teste.js

--- teste.js
+++ teste.js
@@ -2,22 +2,28 @@ var html = document.querySelector('.janela').innerHTML;
 document.getElementById('html').querySelector('code').innerText = html;
 
 function display() {
-    elemento = document.getElementById("display").value
+    elementodisplay = document.getElementById("display").value
     var elements = {
         "flex": "flex_on",
         "grid": "grid_on",
         "block": "block_on",
         "inline": "inline_on",
         "inline-block": "inline_block_on",
-        "none": "none_on",
+
         "table": "table_on",
         "inline-flex": "inline_flex_on",
-        
+
     };
 
     for (var key in elements) {
         var element = document.getElementById(elements[key]);
-        element.style.display = (key === elemento) ? key : "none";
+
+        if (key === elementodisplay) {
+            element.style.display = key;
+            element.style.display = "block";
+        } else {
+            element.style.display = "none";
+        }
     }
 }
 
@@ -29,7 +35,7 @@ function background() {
         "image": "image_on",
         "size": "size_on",
         "repeat": "repeat_on",
-        
+
     };
 
     for (var key in elements) {
@@ -40,34 +46,15 @@ function background() {
 
 
 function border() {
-    var elemento = document.getElementById("border").value;
-    var elements = {
-        "width": "width_on",
-        "style": "style_on",
-        "color": "color_on",
-        "radius": "radius_on",
-        "full": "full_on",
-        "top": "top_on",
-        "right": "right_on",
-        "bottom": "bottom_on",
-        "left": "left_on",
-        "image": "image_on",
-        "collapse": "collapse_on",
-        "spacing": "spacing_on",
-        "outline": "outline_on",
-        "top-left-radius": "top_left_radius_on",
-        "top-right-radius": "top_right_radius_on",
-        "bottom-left-radius": "bottom_left_radius_on",
-        "bottom-right-radius": "bottom_right_radius_on"
-        
-    };
-
-    for (var key in elements) {
-        var element = document.getElementById(elements[key]);
-        element.style.display = (key === elemento) ? key : "none";
+    elemento = document.getElementById("elemento").value
+    var elementoborder = document.getElementById("border").value;
+    if (elementoborder === "ativa" && elemento !== 'null') {
+        document.getElementById('border_on').style.display = 'block';
+    }
+    else {
+        document.getElementById('border_on').style.display = 'none';
     }
 }
-
 
 function margin() {
     var elemento = document.getElementById("border").value;
@@ -86,21 +73,34 @@ function margin() {
 }
 
 function global(atributo) { // atributo do css que vai ser aplicado
-
     elemento = document.getElementById("elemento").value // elemento que vai receber atributo
-    const css = document.getElementById(atributo).value // valor que o tibuto vai receber
-    if (document.getElementById(elemento + atributo)) {// este if verifica se existe a linha pra modiificar o texto
-        document.getElementById(elemento + atributo).innerText = atributo + ":" + css + ";"; // css vai ser escrito no navegador
+
+    if (document.getElementById(atributo).value === "ativa" && elemento !== 'null') {
+        return;
     }
-    else { // este else adiciona a linha ao corpo do site
-        var criar = document.createElement('p');
-        criar.id = elemento + atributo;
-        document.getElementById('visor' + elemento).appendChild(criar)
-        document.getElementById(elemento + atributo).innerText = atributo + ":" + css + ";";
-        document.getElementById(elemento + atributo).style = 'color: rgb(3, 143, 194);';
-        document.getElementById(elemento + atributo).style.marginLeft = '15px';
+    else {
+        const css = document.getElementById(atributo).value // valor que o tibuto vai receber
+        if (document.getElementById(atributo).value === "null") {
+            document.getElementById(elemento + atributo).style.display = "none";
+        }
+        else if (document.getElementById(elemento + atributo)) {// este if verifica se existe a linha pra modiificar o texto
+            document.getElementById(elemento + atributo).innerText = atributo + ":" + css + ";"; // css vai ser escrito no navegador
+            document.getElementById(elemento + atributo).style.display = "block";
+
+        }
+
+
+
+        else { // este else adiciona a linha ao corpo do site
+            var criar = document.createElement('p');
+            criar.id = elemento + atributo;
+            document.getElementById('visor' + elemento).appendChild(criar)
+            document.getElementById(elemento + atributo).innerText = atributo + ":" + css + ";";
+            document.getElementById(elemento + atributo).style = 'color: rgb(3, 143, 194);';
+            document.getElementById(elemento + atributo).style.marginLeft = '15px';
+        }
+        document.getElementById(elemento).style[atributo] = css;//adicionando o valor selecionado ao atributo do elemento escolhido
     }
-    document.getElementById(elemento).style[atributo] = css;//adicionando o valor selecionado ao atributo do elemento escolhido
 }
 function criarelemento() {
     var add = document.getElementById('criadorelemento').value;// input nome do elemento
